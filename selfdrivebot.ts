a89501a611d67b612d359c53c7c7bc661558a016
Israel Torres <israel.torres@kapelse.com>
2019-06-05 18:16:00 +0200

    third update

--- selfdrivebot.ts
+++ selfdrivebot.ts
@@ -22,6 +22,37 @@ namespace selfdrivebot {
         Tracking_State_3,
     }
 
+    export enum SpeedStateType {
+        //% block="● ◌ ◌ ◌" enumval=0
+        Speed_State_0,
+
+        //% block="◌ ● ◌ ◌" enumval=1
+        Speed_State_1,
+
+        //% block="◌ ◌ ● ◌" enumval=2
+        Speed_State_2,
+
+        //% block="◌ ◌ ◌ ●" enumval=3
+        Speed_State_3,
+    }
+
+    export enum SteerStateType {
+        //% block="● ◌ ◌ ◌ ◌" enumval=0
+        Steer_State_0,
+
+        //% block="◌ ● ◌ ◌ ◌" enumval=1
+        Steer_State_1,
+
+        //% block="◌ ◌ ● ◌ ◌" enumval=2
+        Steer_State_2,
+
+        //% block="◌ ◌ ◌ ● ◌" enumval=3
+        Steer_State_3,
+
+        //% block="◌ ◌ ◌ ◌ ●" enumval=4
+        Steer_State_4
+    }
+
 
     export enum Distance_Unit {
         //% block="mm" enumval=0
@@ -35,45 +66,59 @@ namespace selfdrivebot {
     }
 
     let pin_run_motor = AnalogPin.P1
-    let pin_steer_motor = AnalogPin.P2
-    let pin_ultrasound = AnalogPin.P10
-    let pin_tracker_left = AnalogPin.P8
-    let pin_tracker_right = AnalogPin.P9
+    let pin_steer_motor = AnalogPin.P14
+    let pin_ultrasound_trig = AnalogPin.P10
+    let pin_ultrasound_echo = AnalogPin.P3
+    let pin_tracker_left = DigitalPin.P8
+    let pin_tracker_right = DigitalPin.P9
 
     /**
-    * TODO: initialization selfdrivebot car control
+    * initialization selfdrivebot car control
     * @param run run motor pin, ex: AnalogPin.P1
     * @param steer steer motor pin, ex: AnalogPin.P2
     */
     //% weight=10
-    //% blockId=selfdrivebot_init_control block="set run motor at pin %run| and steer motor at pin %steer"
-    export function init_control(run: AnalogPin, steer: AnalogPin): void {
+    //% blockId=selfdrivebot_init_run block="set run motor at pin %run"
+    export function init_run(run: AnalogPin): void {
         // Add code here
         pin_run_motor = run
+
+    }
+
+    /**
+    * initialization selfdrivebot car steer pin
+    * @param steer steer motor pin, ex: AnalogPin.P2
+    */
+    //% weight=10
+    //% blockId=selfdrivebot_init_steer block="set steer motor at pin %steer"
+    export function init_steer(steer: AnalogPin): void {
+        // Add code here
         pin_steer_motor = steer
 
     }
 
     /**
-    * TODO: initialization selfdrivebot car ultrasound sensor
-    * @param ultrasound ultrasound sensor pin, ex: AnalogPin.P10
+    * initialization selfdrivebot car ultrasound sensor
+    * @param trig trigger ultrasound sensor pin, ex: AnalogPin.P3
+    * @param echo echo ultrasound sensor pin, ex: AnalogPin.P10
     */
     //% weight=10
-    //% blockId=selfdrivebot_init_ultrasound block="set ultrasound sensor at %ultrasound"
-    export function init_us_sensor(ultrasound: AnalogPin): void {
+    //% blockId=selfdrivebot_init_ultrasound block="set ultrasound trigger at %trig| and echo at %echo"
+    export function init_us_sensor(trig: AnalogPin, echo: AnalogPin): void {
         // Add code here
-        pin_ultrasound = ultrasound
+        pin_ultrasound_trig = trig
+        pin_ultrasound_echo = echo
 
     }
 
     /**
-    * TODO: initialization selfdrivebot car tracking sensors
+    * initialization selfdrivebot car tracking sensors
     * @param left left tracking sensor pin, ex: AnalogPin.P8
     * @param right right tracking sensor pin, ex: AnalogPin.P9
     */
     //% weight=10
     //% blockId=selfdrivebot_init_tracking block="set left tracking sensor at %left| and right at %right"
-    export function init_tracking_sensors(left: AnalogPin, right: AnalogPin): void {
+    export function init_tracking_sensors(left: DigitalPin, right: DigitalPin): void {
         // Add code here
         pin_tracker_left = left
         pin_tracker_right = right
@@ -81,29 +126,43 @@ namespace selfdrivebot {
     }
 	
 	/**
-	* Runs the motor at the given speed
+	* Moves forward the motor at the given speed
 	*/
 	//% block="crickit run at $speed \\%"
-    //% speed.shadow="speedPicker"
-    //% blockId=selfdrivebot_run block="run at %speed"
-	export function run(speed: number) {
-        pins.servoSetPulse(pin_run_motor, 2400)
-        pins.servoSetPulse(pin_steer_motor, 600)
+    //% blockId=selfdrivebot_move_forward block="move forward at %speed"
+	export function move_forward(speed: SpeedStateType) {
+
+    }
+    
+    /**
+	* Moves backward the motor at the given speed
+	*/
+	//% block="crickit run at $speed \\%"
+    //% blockId=selfdrivebot_move_backward block="move backward at %speed"
+	export function move_backward(speed: SpeedStateType) {
+
+    }
+    
+    /**
+	* Brakes the motor
+	*/
+    //% blockId=selfdrivebot_brake block="brake"
+	export function brake() {
+
 	}
 	
 	/**
 	* Steers two motors by the given ratio
 	*/
 	//% block="steer $turnRatio"
-	//% turnRatio.min=-200 turnRatio.max=200
-    //% turnratio.shadow=turnRatioPicker
     //% blockId=selfdrivebot_steer block="steer at %turnRatio"
-	export function steer(turnRatio: number) {
+	export function steer(turnRatio: SteerStateType) {
+        pins.servoSetPulse(pin_steer_motor, 600)
 	}
 
 
     /**
-    * TODO: line following
+    * line following
     */
     //% weight=10
     //% advanced=true
@@ -117,7 +176,7 @@ namespace selfdrivebot {
 
 
     /**
-    * TODO: get ultrasonic distance
+    * get ultrasonic distance
     */
     //% weight=9
     //% advanced=true
@@ -131,3 +190,4 @@ namespace selfdrivebot {
 
 
 }
+
